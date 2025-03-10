020b95243883335b231c77f9faf31a93bc8cacce
Dmitry Nikiforov <nikfcsm@gmail.com>
2025-03-10 12:38:11 +0300

    4

---
+++ about.php
@@ -0,0 +1,128 @@
+<?php include("header.php") ?>
+<section id="about_us__photo">
+    <div class="container">
+        <h2>Здесь почти весь <span>наш состав</span></h2>
+        <div class="about_photo__wrapper ">
+            <!-- <div><img src="img/about/about1.jpg" alt=""></div> -->
+            <div class="w100"><img src="img/about/about2.jpg" alt=""></div>
+        </div>
+        <h2>Лучшие <span>аниматоры</span></h2>
+        <div class="about_photo__wrapper">
+            <div><img src="img/about/about3.jpg" alt=""></div>
+            <div><img src="img/about/about7.jpg" alt=""></div>
+        </div>
+        <h2>Лучшие <span>инструктора</span></h2>
+        <div class="about_photo__wrapper">
+            <div><img src="img/about/about5.jpg" alt=""></div>
+            <div><img src="img/about/about6.jpg" alt=""></div>
+        </div>
+    </div>
+</section>
+<section id="about_us__number">
+    <div class="container">
+        <h2>Посмотрим <span>на цифры</span></h2>
+        <div class="about_number__wrapper">
+            <div class="about_number__item">
+                <span>3000</span>
+                <div class="about_us__facts">
+                    Детей
+                </div>
+                <p>бегают в наши анимационные программмы за сезон</p>
+            </div>
+            <div class="about_number__item">
+                <span>300</span>
+                <div class="about_us__facts">
+                Программ
+                </div>
+                <p>мы проводим за сезон</p>
+            </div>
+            <div class="about_number__item">
+                <span>150</span>
+                <div class="about_us__facts">
+                Человек
+                </div>
+                <p>трудятся в Парке, чтобы вам было незабываемо весело</p>
+            </div>
+            <div class="about_number__item">
+                <span>20-30</span>
+                <div class="about_us__facts">
+                Инструкторов
+                </div>
+                <p>на трассах еревочного парка работают ежедневно</p>
+            </div>
+            <div class="about_number__item">
+                <span>5.0</span>
+              
+                <p>Рейтинг в Яндекс Картах</p>
+                <div class="about3_raiting page_raiting">
+                            <img src="/img/home/star_ico.svg" alt="">
+                            <img src="/img/home/star_ico.svg" alt="">
+                            <img src="/img/home/star_ico.svg" alt="">
+                            <img src="/img/home/star_ico.svg" alt="">
+                            <img src="/img/home/star_ico.svg" alt="">
+                        </div>
+                        <a class="about4_link" target="_blank" rel="nofollow" href="https://yandex.ru/profile/1134937550?lang=ru">Убедиться в этом</a>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="about_princip">
+<div class="container">
+    <h2><span>Принцип работы</span> Парка Приключений</h2>
+    <div class="about_princip__grid">
+  <div class="about_page-1">
+    <div class="about_page-1--left">
+        <!-- <img src="img/about/about_page-1bg.jpg" alt=""> -->
+        <picture>
+                            <source srcset="img/about/about_page-1bg-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/about/about_page-1bg.jpg" alt="Абстрактное изображение">
+                        </picture>
+        </div>
+        <div class="about_page-1--right">
+        <h3>Индивидуальный подход</h3>
+        <p>Мы обеспечиваем индивидуальный подход к каждому гостю и группе отдыхающих</p>
+        <p style="color:#9593A0;">Мы не впариваем, а лишь даем совет, как лучше провести свой день</p>
+        <div class="contact_us__wrapper-groupbtn">
+                            <a class="contact_us-btn" href="#zayavka">Оставить заявку</a>
+                            <a rel="nofollow" href="https://vk.com/adventurepark"><img src="/img/vk.svg" alt=""></a>
+                            <a rel="nofollow" href="https://wa.me/79150210989"><img src="/img/wa.svg" alt=""></a>
+                            <a rel="nofollow" href="https://t.me/adventureparkru"><img src="/img/tg.svg" alt=""></a>
+                        </div>
+        </div>
+
+
+  </div>
+  <div class="about_page-2">
+  В нашем парке все работает идеально. Мы встретим гостей, проводим до беседки, а попутно расскажем обо всем необходимом, что важно знать отдыхая у нас
+  </div>
+  <div class="about_page-3">
+    <span>Мы предлагаем варианты отдыха и программы как у Яндекс Такси у нас есть варианты</span>
+    <div class="stickers_wrapper">
+    <div class="stickers">
+    Эконом
+    </div>
+    <div class="stickers">
+    Комфорт
+    </div>
+    <div class="stickers">
+    Бизнес
+    </div>
+    </div>
+  </div>
+  <div class="about_page-4">
+    <h3>Мы не безупречны.</h3>
+    <span>Сложные ситуации случаются, как и у всех, кто трудится на полную мощность. Однако, когда это происходит, мы прилагаем все усилия, чтобы исправить ситуацию. Мы не ищем оправданий и не уклоняемся от ответственности - мы вактивно рещаем вознмикшие проблемы</span>
+  </div>
+  <div class="about_page-5">
+    <span>На главную</span>
+    <a href="/"><img src="img/about/about_home.svg" alt=""></a>
+  </div>
+  <div class="about_page-6">
+    <a class="d-mob-none" href="#"><img src="img/about/about_loc.svg" alt=""></a>
+    <a class="d-mob-none" href="#"><img src="img/about/about_navi.svg" alt=""></a>
+    <a class="d-pc-none" href="#"><img src="img/about/about_navi-mob.svg" alt=""></a>
+  </div>
+</div>
+</div>
+</section>
+<?php include("footer.php") ?>
--- park.php
+++ park.php
@@ -1,68 +1,210 @@
-<?php include ("header.php") ?>
+<?php include("header.php") ?>
 <section class="park-showcase">
-<div class="container">
-  <div class="park__showcase_wrapper">
-    <div class="park__showcase_left">
-    <span class="park_showcase__head">Добро пожаловать</span>
+    <div class="container">
+        <div class="park__showcase_wrapper">
+            <div class="park__showcase_left">
+                <span class="park_showcase__head">Добро пожаловать</span>
 
-    <h1>Веревочный парк</h1>
-    <span class="park_showcase__footer">Самый большой веревочный парк Подмосковья, состоящий из 18 трасс разного уровня сложности 
-    и общей длиной маршрутов около 2 км</span>
-    <div class="park_showcase__btns">
-        <a class="animation_item__btn" href="#track">Посмотреть трассы</a>
-        <a class="loc-btn" href=""><img src="/img/location.svg" alt=""></a>
-    </div>
-    </div>
-    <div class="park__showcase_right">
-        <img src="img/park/baner-bg.png" alt="">
+                <h1>Веревочный парк</h1>
+                <span class="park_showcase__footer">Самый большой веревочный парк Подмосковья, состоящий из 18 трасс разного уровня сложности
+                    и общей длиной маршрутов около 2 км</span>
+                <div class="park_showcase__btns">
+                    <a class="animation_item__btn" href="#track">Посмотреть трассы</a>
+                    <a class="loc-btn" href=""><img src="/img/location.svg" alt=""></a>
+                </div>
+            </div>
+            <div class="park__showcase_right">
+                <img src="img/park/baner-bg.png" alt="">
+            </div>
         </div>
-  </div>  
-</div>
+    </div>
 </section>
 <section id="track">
     <div class="container">
         <div class="track_wrapper">
-        <div class="track_item track1">
-            <h2><span>Детские</span> трассы</h2>
-            <div class="animation_item__info">
-                                    <span>От 6 трасс</span>
-                                    <span>до 135см рост</span>
-                                    <span>От 1ч 20м среднее время</span>
-                                </div>
-                                <div class="flex mt20">
-                                <div class="track1_photo"><img src="img/park/track1.png" alt=""></div>
-                                <div class="track1_info">
-                                    <div> <img src="img/park/track1-all.svg" alt=""></div>
-                             
-                                  
-                                
-                                </div>
-                                </div>
-        </div> 
+            <div class="track_item track1">
+                <h2><span>Детские</span> трассы</h2>
+                <div class="animation_item__info">
+                    <span>От 6 трасс</span>
+                    <span>до 135см рост</span>
+                    <span>От 1ч 20м среднее время</span>
+                </div>
+                <div class="flex mt20">
+                    <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track1-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track1.jpg" alt="">
+                        </picture>
+                     </div>
+                        <div class="track1_info">
+                            <div>
+                                <picture>
+                                    <source srcset="img/park/track1-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track1-all.svg" alt="">
+                                </picture>
+                              
+                            </div>
+                            </div>
+                         
 
-        <div class="track_item track2">
-            <h2><span>Детские</span> трассы</h2>
-            <div class="animation_item__info">
-                                    <span>От 6 трасс</span>
-                                    <span>до 135см рост</span>
-                                    <span>От 1ч 20м среднее время</span>
-                                </div>
-                                <div class="flex mt20">
-                                <div class="track1_photo"><img src="img/park/track2.png" alt=""></div>
-                                <div class="track1_info">
-                                    <div> <img src="img/park/track2-n7.svg" alt=""></div>
-                                    <div> <img src="img/park/track2-2.svg" alt=""></div>
-                                    <div> <img src="img/park/track2-3.svg" alt=""></div>
-                                    <div> <img src="img/park/track2-4.svg" alt=""></div>
-                                    <div> <img src="img/park/track2-5.svg" alt=""></div>
-                                    <div> <img src="img/park/track2-6.svg" alt=""></div>
-                                  
-                                
-                                </div>
-                                </div>
-        </div> 
 
+                        </div>
+                    </div>
+                
+
+                <div class="track_item track2">
+                    <h2><span>Легкие</span> трассы</h2>
+                    <div class="animation_item__info">
+                        <span>От 6 трасс</span>
+                        <span>до 135см рост</span>
+                        <span>От 1ч 20м среднее время</span>
+                    </div>
+                    <div class="flex mt20">
+                        <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track2-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track2.jpg" alt="">
+                        </picture>
+                        </div>
+                        <div class="track1_info">
+                            <div>           <picture>
+                                    <source srcset="img/park/track2-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track2-all.svg" alt="">
+                                </picture></div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="track_item track3">
+                    <h2><span>Средние</span> трассы</h2>
+                    <div class="animation_item__info">
+                        <span>От 3 трасс</span>
+                        <span>до 135см рост</span>
+                        <span>От 1ч 20м среднее время</span>
+                    </div>
+                    <div class="flex mt20">
+                        <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track3-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track3.jpg" alt="">
+                        </picture>
+                        </div>
+                        <div class="track1_info">
+                            <div>           <picture>
+                                    <source srcset="img/park/track3-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track3-all.svg" alt="">
+                                </picture></div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="track_item track4">
+                    <h2><span>Сложные</span> трассы</h2>
+                    <div class="animation_item__info">
+                        <span>От 3 трасс</span>
+                        <span>до 135см рост</span>
+                        <span>От 1ч 20м среднее время</span>
+                    </div>
+                    <div class="flex mt20">
+                        <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track4-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track4.jpg" alt="">
+                        </picture>
+                        </div>
+                        <div class="track1_info">
+                            <div>           <picture>
+                                    <source srcset="img/park/track4-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track4-all.svg" alt="">
+                                </picture></div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="track_item track5">
+                    <h2><span>Экстремальные</span> трассы</h2>
+                    <div class="animation_item__info">
+                        <span>От 3 трасс</span>
+                        <span>до 135см рост</span>
+                        <span>От 1ч 20м среднее время</span>
+                    </div>
+                    <div class="flex mt20">
+                        <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track5-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track5.jpg" alt="">
+                        </picture>
+                        </div>
+                        <div class="track1_info">
+                            <div>           <picture>
+                                    <source srcset="img/park/track5-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track5-all.svg" alt="">
+                                </picture></div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="track_item track6">
+                    <h2><span>Троллейная</span> трасса</h2>
+                    <div class="animation_item__info">
+                        <span>От 3 трасс</span>
+                        <span>до 135см рост</span>
+                        <span>От 1ч 20м среднее время</span>
+                    </div>
+                    <div class="flex mt20">
+                        <div class="track1_photo">
+                        <picture class="br30">
+                            <source srcset="img/park/track6-mob.jpg" media="(max-width: 768px)">
+                            <img src="img/park/track6.jpg" alt="">
+                        </picture>
+                        </div>
+                        <div class="track1_info">
+                            <div>          <picture>
+                                    <source srcset="img/park/track6-all-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/track6-all.svg" alt="">
+                                </picture></div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <a class="btn-price " href="/price/"><img src="img/price.svg" alt=""></a>
+        </div>
+</section>
+<?php include("inc/about.php") ?>
+<section id="other_parkactivity">
+    <div class="container">
+        <div class="other_parkactivity__wrapper">
+            <div class="other_parkactivity__item">
+                <h3>Если <span>ребенку</span> будет <span>страшно</span>, у нас есть Скворечник</h3>
+                <picture>
+                                    <source srcset="img/park/other1-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/other1.svg" alt="">
+                                </picture>
+            
+            </div>
+            <div class="other_parkactivity__item">
+                <h3>Если <span>взрослому мало экстрима</span>, у нас есть Tower Park</h3>
+                <picture>
+                                    <source srcset="img/park/other2-mob.svg" media="(max-width: 768px)">
+                                    <img src="img/park/other2.svg" alt="">
+                                </picture>
+            </div>
         </div>
     </div>
+
 </section>
-<?php include ("footer.php") ?>
+<?php include("inc/also.php") ?>
+<?php include("inc/gallery.php") ?>
+<?php include("inc/contact_us.php") ?>
+<?php include("footer.php") ?>
